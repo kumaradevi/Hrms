f35fc3bd31d4969c466d6a7c44f3bae12135ebd9
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-08-03 13:04:08 +0530

    Update Sidebar.jsx

---
+++ hrms/src/components/layout/Sidebar.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import WorkIcon from '@mui/icons-material/Work';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpIcon from '@mui/icons-material/Help';
+import { Link } from 'react-router-dom';
+
+const drawerWidth = 240;
+
+function Sidebar() {
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <List>
+        <ListItem button component={Link} to="/">
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button component={Link} to="/employees">
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Employees" />
+        </ListItem>
+        <ListItem button component={Link} to="/departments">
+          <ListItemIcon>
+            <WorkIcon />
+          </ListItemIcon>
+          <ListItemText primary="Departments" />
+        </ListItem>
+        <ListItem button component={Link} to="/leave/request">
+          <ListItemIcon>
+            <EventNoteIcon />
+          </ListItemIcon>
+          <ListItemText primary="Leave Management" />
+        </ListItem>
+        <ListItem button component={Link} to="/payroll">
+          <ListItemIcon>
+            <AttachMoneyIcon />
+          </ListItemIcon>
+          <ListItemText primary="Payroll" />
+        </ListItem>
+        <ListItem button component={Link} to="/performance/review">
+          <ListItemIcon>
+            <AssessmentIcon />
+          </ListItemIcon>
+          <ListItemText primary="Performance" />
+        </ListItem>
+        <ListItem button component={Link} to="/settings/user">
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+        <ListItem button component={Link} to="/help">
+          <ListItemIcon>
+            <HelpIcon />
+          </ListItemIcon>
+          <ListItemText primary="Help & Support" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+}
+
+export default Sidebar;
