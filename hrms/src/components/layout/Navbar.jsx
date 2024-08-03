41ff0481aeadb2e7e061f0a8ec7d73c7dac6ddc9
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-08-03 13:04:03 +0530

    Update Navbar.jsx
    wrote the code structure

---
+++ hrms/src/components/layout/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+function Navbar() {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          HR Management System
+        </Typography>
+        <IconButton color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
