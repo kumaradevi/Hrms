5cfff28cdccca280c9cd5dd5772f803285aba41b
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-08-04 12:58:40 +0530

    Update Sidebar.jsx

--- hrms/src/components/layout/Sidebar.jsx
+++ hrms/src/components/layout/Sidebar.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   Drawer,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
-} from '@mui/material';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PeopleIcon from '@mui/icons-material/People';
-import WorkIcon from '@mui/icons-material/Work';
-import EventNoteIcon from '@mui/icons-material/EventNote';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import AssessmentIcon from '@mui/icons-material/Assessment';
-import SettingsIcon from '@mui/icons-material/Settings';
-import HelpIcon from '@mui/icons-material/Help';
-import { Link } from 'react-router-dom';
+} from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import WorkIcon from "@mui/icons-material/Work";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HelpIcon from "@mui/icons-material/Help";
+import { Link } from "react-router-dom";
+import Switch from "@mui/material/Switch";
+import { useTheme } from "@emotion/react";
 
 const drawerWidth = 240;
 
-function Sidebar() {
+function Sidebar({ darktheme, toggleTheme }) {
+ 
+  const theme = useTheme();
+  const [mode, setMode] = useState(darktheme.dark);
+
+  const handleThemeChange = () => {
+    setMode(!mode);
+    toggleTheme();
+  };
+
+   
   return (
-    <Drawer
+    <Drawer 
       sx={{
         width: drawerWidth,
+        width:{xs:"100px"},
+        position:"fixed",
         flexShrink: 0,
-        '& .MuiDrawer-paper': {
+        "& .MuiDrawer-paper": {
           width: drawerWidth,
-          boxSizing: 'border-box',
-          marginTop: '65px',
+          boxSizing: "border-box",
+          marginTop: "65px",
         },
       }}
       variant="permanent"
@@ -81,6 +95,16 @@ function Sidebar() {
             <HelpIcon />
           </ListItemIcon>
           <ListItemText primary="Help & Support" />
+        </ListItem>
+        <ListItem>
+          <Switch onClick={handleThemeChange}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            defaultChecked
+          />
         </ListItem>
       </List>
     </Drawer>
